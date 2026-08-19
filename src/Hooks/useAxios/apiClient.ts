24b67ea5619d32e1020ef6ff4bebed93d539758d
tdigital <Administrador@FBOLIVAR.LOCAL>
2026-08-19 16:53:02 -0500

    Task: Update build files

--- src/Hooks/useAxios/apiClient.ts
+++ src/Hooks/useAxios/apiClient.ts
@@ -2,11 +2,11 @@ import axios, { type AxiosInstance } from 'axios';
 
 
 const apiClient: AxiosInstance = axios.create({
-    //baseURL: "http://10.1.1.14:8000/",
+    baseURL: "http://10.1.1.14:8000/",
     //import.meta.env.VITE_API_BASE_URL
     //baseURL: "http://localhost:8000",
     //baseURL: "http://10.1.0.226:8000/",
-    baseURL: "http://127.0.0.1:8000/",
+    //baseURL: "http://127.0.0.1:8000/",
     headers: {
         'Content-Type': 'application/json', //APIrest common response json type
     },
